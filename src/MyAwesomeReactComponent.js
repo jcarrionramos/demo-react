c086c0e4490e9c1ffc4275782b17b97dd3e54900
jcarrionramos <pollo_carrion@hotmail.com>
2017-05-05 15:57:13 -0300

    No icons yet

--- src/MyAwesomeReactComponent.js
+++ src/MyAwesomeReactComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
-import FontIcon from 'material-ui/FontIcon';
 import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
 import MenuItem from 'material-ui/MenuItem';
 import DropDownMenu from 'material-ui/DropDownMenu';
@@ -40,7 +39,6 @@ export default class ToolbarExamplesSimple extends React.Component {
         </ToolbarGroup>
         <ToolbarGroup>
           <ToolbarTitle text="Options" />
-          <FontIcon className="muidocs-icon-custom-sort" />
           <ToolbarSeparator />
           <RaisedButton label="Create Broadcast" primary={true} />
           <IconMenu
